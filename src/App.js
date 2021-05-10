ff5e5d25c9bd82a55ae6479287c9f1739b5ad4b3
psd90 <pdhillon.dev@gmail.com>
2021-05-10 02:35:13 +0100

    fifth commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import {Switch, Route, HashRouter} from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 //pages
 import Home from './pages/home';
 import About from './pages/about';
@@ -19,7 +19,7 @@ import './App.scss';
 const App = () => {
   return (
     <div className="App">
-      <HashRouter>
+      <BrowserRouter>
       <Switch>
       <Route exact path="/" render={() => (
         <Home />
@@ -52,7 +52,7 @@ const App = () => {
           <Web />
         )}/>
       </Switch>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
